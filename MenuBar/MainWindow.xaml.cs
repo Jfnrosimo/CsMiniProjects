b8c9b6c4899cd5749fa9d9a00059dd6fbe9bd299
Jfnrosimo <j.rosimo098.com>
2023-02-18 19:08:26 +0800

    Feat: added menu items and a message box

--- MenuBar/MainWindow.xaml.cs
+++ MenuBar/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MenuBar
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New Project clicked!");
+        }
     }
 }
