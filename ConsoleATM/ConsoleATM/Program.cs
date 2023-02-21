d2c8b517f3aadeed2db2be41b0ca6874a9c2bcd0
Jfnrosimo <j.rosimo098.com>
2023-02-21 11:05:27 +0800

    Feat: added deposit and withdraw methods

--- ConsoleATM/ConsoleATM/Program.cs
+++ ConsoleATM/ConsoleATM/Program.cs
@@ -41,4 +41,64 @@ public class cardHolder
     {
         return balance;
     }
+
+    public void setCardNum(String newCardNum)
+    {
+        cardNum = newCardNum;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+
+    public void setFirstName(String newFirstName)
+    {
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName)
+    {
+        lastName = newLastName; 
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+
+    public static void Main(String[] args)
+    {
+        void printOptions()
+        {
+            Console.WriteLine("Please choose your options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to deposit?: ");
+            double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit + currentBalance);
+            Console.WriteLine("Thank you for your deposit. Your new balance is "
+                + currentUser.getBalance());
+        }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to withdraw?: ");
+            double withdraw = Double.Parse(Console.ReadLine());
+            if(currentUser.getBalance() > withdraw)
+            {
+                Console.WriteLine("Insufficient balance: ");
+            }
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdraw);
+            }
+        }
+    }
 }
