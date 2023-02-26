1da22eae19ab90a5a0bac1c6699c92c8f268edc9
Jfnrosimo <j.rosimo098.com>
2023-02-26 16:36:13 +0800

    Feat: fix ambiguity on winforms and WPF

---
+++ DialogBoxFunc/DialogBoxFunc/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Winforms = System.Windows.Forms; //Initialize winforms to avoid abiguity
+
+namespace DialogBoxFunc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+            dialog.ShowDialog();
+
+            //Ambiguity issue on winforms and WPF
+            MessageBox.Show();
+        }
+
+        
+    }
+}
