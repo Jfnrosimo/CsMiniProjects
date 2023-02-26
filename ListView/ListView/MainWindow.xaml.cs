baa3cd6219936cbfeb496169d54cf53e8cef8a60
Jfnrosimo <j.rosimo098.com>
2023-02-26 17:33:59 +0800

    Feat: added delete on list view

--- ListView/ListView/MainWindow.xaml.cs
+++ ListView/ListView/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,15 +24,40 @@ namespace ListView
         public MainWindow()
         {
             InitializeComponent();
-            lvEntries.Items.Add("a");
-            lvEntries.Items.Add("a1");
-            lvEntries.Items.Add("a2");
-            lvEntries.Items.Add("a3");
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             lvEntries.Items.Add(txtEntry.Text);
+            txtEntry.Clear();
+            txtEntry.Focus();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            //Using only on single selection
+            //int index = lvEntries.SelectedIndex;
+
+            //Using object on single collection
+            //object item = lvEntries.SelectedItem;
+
+            //Using multiple selection
+            var items = lvEntries.SelectedItems;
+
+            var result = MessageBox.Show($"Are you sure you want to delete: {items.Count} ?", "Delete", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                var itemList = new ArrayList(items);
+                foreach (var item in itemList)
+                {
+                    lvEntries.Items.Remove(item);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Clear();
         }
     }
 }
