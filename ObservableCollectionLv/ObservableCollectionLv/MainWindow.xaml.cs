041e23e7da134928090ecbb83f0fcf7218592028
Jfnrosimo <j.rosimo098.com>
2023-02-27 09:34:49 +0800

    Feat: added item source binding

--- ObservableCollectionLv/ObservableCollectionLv/MainWindow.xaml.cs
+++ ObservableCollectionLv/ObservableCollectionLv/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ObservableCollectionLv
 {
@@ -22,7 +10,33 @@ namespace ObservableCollectionLv
     {
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+            entries = new ObservableCollection<string>();
+        }
+
+        private ObservableCollection<string> entries;
+
+        public ObservableCollection<string> Entries
+        {
+            get { return entries; }
+            set { entries = value; }
+        }
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
